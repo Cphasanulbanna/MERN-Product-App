b2b6a6dfd0697a1a2c0aef9ec2e0b79175b99ea8
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-23 21:07:06 +0530

    all products api completed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
+//modules
 const express = require("express");
 const cors = require("cors");
 
+const products = require("./products.json");
+
 const app = express();
 app.use(cors());
-const PORT = 7000;
+const PORT = 8000;
+
+//get all products api
+app.get("/products", (req, res) => {
+    res.status(200).json({ products: products });
+});
+
+//error route
+app.all("*", (req, res) => {
+    res.status(404).json({ message: "404 page not found" });
+});
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
