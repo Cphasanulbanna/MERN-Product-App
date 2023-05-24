1a58eb7cabf714931fbb0f1d300b6660eaaf1c83
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-24 13:40:21 +0530

    orders api completed

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const products = require("./products.json");
 let cart = [];
+let orders = [];
 
 const app = express();
 app.use(express.json());
@@ -62,6 +63,30 @@ app.delete("/cart", (req, res) => {
     } else {
         res.status(200).json({ message: `cart is empty , please add some products` });
     }
+});
+
+//order product api
+app.post("/order", (req, res) => {
+    const { id, quantity } = req.body;
+    if (!id) return res.status(400).json({ message: "product id is required" });
+
+    const productToBuy = products.find((product) => product.id == id);
+    productToBuy.quantity = quantity ? quantity : 1;
+    productToBuy["total"] = quantity ? quantity * productToBuy.price : productToBuy.price;
+    orders.push(productToBuy);
+
+    res.status(200).json({
+        totalAmount: quantity ? `$ ${productToBuy?.total}` : productToBuy.price,
+        message: `order placed for product with id:${id}`,
+        orderedProduct: productToBuy,
+    });
+});
+
+//fetch order history
+app.get("/orders", (req, res) => {
+    if (orders.length === 0)
+        return res.status(200).json({ message: "You are not ordered any product", orders: orders });
+    res.status(200).json({ message: "Success", products: orders });
 });
 
 //error route
